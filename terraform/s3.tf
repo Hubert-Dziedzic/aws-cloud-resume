6d1b422f01be3d924599056407858d34a086f445
Hubert-Dziedzic <hubertdziedzic26@gmail.com>
2026-05-27 09:18:38 +0200

    create s3 bucket for web

---
+++ terraform/s3.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "cloud_portfolio_bucket" {
+    bucket = "cloud-portfolio-bucket-hd-20026"
+}
+
+resource "aws_s3_bucket_public_access_block" "cloud_portfolio_public_access_block" {
+    bucket = aws_s3_bucket.cloud_portfolio_bucket.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+}
+
