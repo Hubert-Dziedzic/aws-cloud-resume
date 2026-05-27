d5e7dbaac6a2628a33575486e976d6b0877ec4b5
Hubert-Dziedzic <hubertdziedzic26@gmail.com>
2026-05-27 09:08:57 +0200

    update .gitignore | add tf provider and s3 backend

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tfstate-sandbox-hd-1337"
+    key    = "aws-cloud-resume/terraform.tfstate"
+    region = "eu-west-3"
+    use_lockfile = true
+  }
+  required_version = ">= 1.10"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
