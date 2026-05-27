659a81ab73620a70dd23150757ce5d0e94249c5b
Hubert-Dziedzic <hubertdziedzic26@gmail.com>
2026-05-27 10:23:53 +0200

    pipeline test

--- terraform/github_oidc.tf
+++ terraform/github_oidc.tf
@@ -53,3 +53,7 @@ resource "aws_iam_policy" "github_actions_deploy_policy" {
   policy = data.aws_iam_policy_document.github_actions_policy.json
 }
 
+resource "aws_iam_role_policy_attachment" "github_actions_attach" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = aws_iam_policy.github_actions_deploy_policy.arn
+}
