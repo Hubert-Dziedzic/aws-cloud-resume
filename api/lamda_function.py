6f97f3603d4628b0f9a70c868f69ef97a3abd00f
Hubert-Dziedzic <hubertdziedzic26@gmail.com>
2026-06-02 12:43:39 +0200

    created lambda function to count views

---
+++ api/lamda_function.py
@@ -0,0 +1,44 @@
+import json
+import boto3
+import os
+
+dynamodb = boto3.resource('dynamodb')
+sns = boto3.client('sns')
+
+TABLE_NAME = os.environ.get('TABLE_NAME', 'VisitorCount')
+TOPIC_ARN = os.environ.get('TOPIC_ARN')
+MY_IP = os.environ.get('MY_IP')
+
+def lambda_handler(event, context):
+    table = dynamodb.Table(TABLE_NAME)
+
+    caller_ip = event.get('requestContext', {}).get('http', {}).get('sourceIp', 'Unknown')
+
+    try:
+        reqsponse = table.update_item(
+            Key={'id': 'views'},
+            UpdateExpression='ADD visits :inc',
+            ExpressionAttributeValues={':inc': 1},
+            ReturnValues='UPDATED_NEW'
+        )
+        current_views = int(reqsponse['Attributes']['visits'])
+    except Exception as e:
+        print(f"Error updating DynamoDB: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Could not update visitor count'})
+        }
+    if caller_ip != MY_IP:
+        try:
+            sns.publish(
+                TopicArn=TOPIC_ARN,
+                subject='New Visitor Alert',
+                Message=f"New visitor from IP: {caller_ip}. Total visits: {current_views}"
+            )
+        except Exception as e:
+            print(f"Error publishing to SNS: {e}")
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'visits': current_views})
+    }
+
